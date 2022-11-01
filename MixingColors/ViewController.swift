c87c78767e63a53c061a1b390a86e852fabee3dd
Elena <elmikhailova86@gmail.com>
2022-11-01 23:17:02 +0300

    final version

--- MixingColors/ViewController.swift
+++ MixingColors/ViewController.swift
@@ -13,10 +13,6 @@ class ViewController: UIViewController {
     
     @IBOutlet var colorView: UIView!
     
-    @IBOutlet var redLabel: UILabel!
-    @IBOutlet var greenLabel: UILabel!
-    @IBOutlet var blueLabel: UILabel!
-    
     @IBOutlet var redLabelValue: UILabel!
     @IBOutlet var greenLabelValue: UILabel!
     @IBOutlet var blueLabelValue: UILabel!
@@ -29,75 +25,52 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        setupRedSlider()
-        setupGreenSlider()
-        setupBlueSlider()
-        
-        setupRedlabelValue()
-        setupGreenlabelValue()
-        setupBluelabelValue()
-        
         colorView.layer.cornerRadius = 15
-        
-        redLabel.textColor = .red
-        greenLabel.textColor = .green
-        blueLabel.textColor = .blue
-        
+        setColor()
+        setValue(for: redLabelValue, greenLabelValue, blueLabelValue)
     }
     
     // MARK: - IB Actions
-    
-    @IBAction func redSliderAction() {
-        redLabelValue.text =  String(format: "%.02f", redSlider.value)
-    }
-    
-    @IBAction func greenSliderAction() {
-        greenLabelValue.text = String(format: "%.02f", greenSlider.value)
-    }
-    
-    @IBAction func blueSliderAction() {
-        blueLabelValue.text = String(format: "%.02f", blueSlider.value)
-    }
-    
-    @IBAction func rgbColors() {
-        viewBackgroundcolor()
+        
+    @IBAction func rgbSliderAction(_ sender: UISlider) {
+        setColor()
+        switch sender {
+        case redSlider:
+            redLabelValue.text = string(from: redSlider)
+        case greenSlider:
+            greenLabelValue.text = string(from: greenSlider)
+        default:
+            blueLabelValue.text = string(from: blueSlider)
+        }
     }
     
     // MARK: - Private Methods
     
-    private func setupRedSlider() {
-        redSlider.minimumTrackTintColor = .red
-        redSlider.value = 0
-        redSlider.maximumValue = 1
-        redSlider.minimumValue = 0
+    private func setColor() {
+        colorView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value), alpha: 1
+        )
+    }
+    
+    private func setValue(for labels: UILabel...) {
+        labels.forEach { label in
+            switch label  {
+            case redLabelValue:
+                redLabelValue.text = string(from: redSlider)
+            case greenLabelValue:
+                greenLabelValue.text = string(from: greenSlider)
+            default:
+                blueLabelValue.text = string(from: blueSlider)
+                
+            }
+        }
         
     }
-    private func setupGreenSlider() {
-        greenSlider.minimumTrackTintColor = .green
-        greenSlider.minimumValue = 0
-        greenSlider.maximumValue = 1
-        greenSlider.value = 0
-    }
-    private func setupBlueSlider() {
-        blueSlider.minimumTrackTintColor = .blue
-        blueSlider.minimumValue = 0
-        blueSlider.maximumValue = 1
-        blueSlider.value = 0
-    }
-    private func setupRedlabelValue() {
-        redLabelValue.textColor = .red
-        redLabelValue.text = redSlider.value.formatted()
-    }
-    private func setupGreenlabelValue() {
-        greenLabelValue.textColor = .green
-        greenLabelValue.text = greenSlider.value.formatted()
-    }
-    private func setupBluelabelValue() {
-        blueLabelValue.textColor = .blue
-        blueLabelValue.text = blueSlider.value.formatted()
-    }
-    private func viewBackgroundcolor() {
-        colorView.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
+    private func string(from slider: UISlider) -> String {
+        String(format: "%.02f", slider.value)
     }
 }
 
+ 
